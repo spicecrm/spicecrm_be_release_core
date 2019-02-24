d6068045eea3481971e0fe844df429e6a9898e1b
maretval <valerie.maret@twentyreasons.com>
2019-02-24 23:08:55 +0100

    Preparing release_201902001

--- sugar_version.php
+++ sugar_version.php
@@ -1,9 +1,9 @@
 <?php
  if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
-$sugar_version      = '2019.01.001';
-$sugar_db_version   = '2019.01.001'; //deprecated since spicecrm 20180100
+$sugar_version      = '2019.02.001';
+$sugar_db_version   = '2019.02.001'; //deprecated since spicecrm 20180100
 $sugar_flavor       = 'CE';
 $sugar_build	    = '1';
-$sugar_timestamp    = '2019-01-11 12:00am';
+$sugar_timestamp    = '2019-02-24 12:00am';
 
