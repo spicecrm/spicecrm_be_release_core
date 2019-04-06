84727a8bf4b9449dd678cb1e5d1714aeb49470ba
maretval <valerie.maret@twentyreasons.com>
2019-04-07 01:29:49 +0200

    Preparing release 201904001: CR1000189

--- include/utils/LogicHook.php
+++ include/utils/LogicHook.php
@@ -1,39 +1,6 @@
 <?php
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
-/*********************************************************************************
-* SugarCRM Community Edition is a customer relationship management program developed by
-* SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
-* 
-* This program is free software; you can redistribute it and/or modify it under
-* the terms of the GNU Affero General Public License version 3 as published by the
-* Free Software Foundation with the addition of the following permission added
-* to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
-* IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
-* OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
-* 
-* This program is distributed in the hope that it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-* FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
-* details.
-* 
-* You should have received a copy of the GNU Affero General Public License along with
-* this program; if not, see http://www.gnu.org/licenses or write to the Free
-* Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-* 02110-1301 USA.
-* 
-* You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
-* SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
-* 
-* The interactive user interfaces in modified source and object code versions
-* of this program must display Appropriate Legal Notices, as required under
-* Section 5 of the GNU Affero General Public License version 3.
-* 
-* In accordance with Section 7(b) of the GNU Affero General Public License version 3,
-* these Appropriate Legal Notices must retain the display of the "Powered by
-* SugarCRM" logo. If the display of the logo is not reasonably feasible for
-* technical reasons, the Appropriate Legal Notices must display the words
-* "Powered by SugarCRM".
-********************************************************************************/
+/***** SPICE-SUGAR-HEADER-SPACEHOLDER *****/
 
 
 /**
@@ -126,15 +93,15 @@ class LogicHook
     {
         global $sugar_config;
 
-        // config|_override.php
-        if (is_file('config.php')) {
-            require_once('config.php'); // provides $sugar_config
-        }
-
-        // load up the config_override.php file.  This is used to provide default user settings
-        if (is_file('config_override.php')) {
-            require_once('config_override.php');
-        }
+//        // config|_override.php
+//        if (is_file('config.php')) {
+//            require_once('config.php'); // provides $sugar_config
+//        }
+//
+//        // load up the config_override.php file.  This is used to provide default user settings
+//        if (is_file('config_override.php')) {
+//            require_once('config_override.php');
+//        }
 
 
         if(!is_null($GLOBALS['db'])){
@@ -164,19 +131,19 @@ class LogicHook
         }
 
         // load from database
-        if(is_null($dbhooks) && empty( $GLOBALS['installing'] )) {
-            if(!class_exists('DBManagerFactory', false)) {
-                if(is_null($GLOBALS['log'])){
-                    require_once "include/SugarLogger/LoggerManager.php";
-                    $GLOBALS['log'] = LoggerManager::getLogger('SugarCRM');
-                }
-                require_once "include/TimeDate.php";
-                require_once "include/database/DBManagerFactory.php";
-            }
-            $dbhooks = DBManagerFactory::getInstance();
-        }
+//        if(is_null($dbhooks) && empty( $GLOBALS['installing'] )) {
+//            if(!class_exists('DBManagerFactory', false)) {
+//                if(is_null($GLOBALS['log'])){
+//                    require_once "include/SugarLogger/LoggerManager.php";
+//                    $GLOBALS['log'] = LoggerManager::getLogger('SugarCRM');
+//                }
+//                require_once "include/TimeDate.php";
+//                require_once "include/database/DBManagerFactory.php";
+//            }
+//            $dbhooks = DBManagerFactory::getInstance();
+//        }
         $SpiceCRMHooks = [];
-        if( empty( $GLOBALS['installing'] )) {
+        if( !is_null($dbhooks) && empty( $GLOBALS['installing'] )) {
 
             if(empty($module_dir)){
 
@@ -186,7 +153,7 @@ class LogicHook
                     $hooks = $dbhooks->query("SELECT event, hook_index, hook_include, hook_class, hook_method FROM syshooks WHERE ( module = '' OR module IS NULL ) AND hook_active = 1 UNION SELECT event, hook_index, hook_include, hook_class, hook_method FROM syscustomhooks WHERE ( module = '' OR module IS NULL ) AND hook_active = 1");
 
                     while ($hook = $dbhooks->fetchByAssoc($hooks)) {
-                        $SpiceCRMHooks[] = array($hook['hook_index'], '', $hook['hook_include'], $hook['hook_class'], $hook['hook_method']);
+                        $SpiceCRMHooks[$hook['event']][] = array($hook['hook_index'], '', $hook['hook_include'], $hook['hook_class'], $hook['hook_method']);
                     }
 
                     // write to the session to speed up performance
@@ -199,7 +166,7 @@ class LogicHook
                     $hooks = $dbhooks->query("SELECT event, hook_index, hook_include, hook_class, hook_method FROM syshooks WHERE module='$module_dir' AND hook_active = 1 UNION SELECT event, hook_index, hook_include, hook_class, hook_method FROM syscustomhooks WHERE module='$module_dir' AND hook_active = 1");
 
                     while ($hook = $dbhooks->fetchByAssoc($hooks)) {
-                        $SpiceCRMHooks[] = array($hook['hook_index'], '', $hook['hook_include'], $hook['hook_class'], $hook['hook_method']);
+                        $SpiceCRMHooks[$hook['event']][] = array($hook['hook_index'], '', $hook['hook_include'], $hook['hook_class'], $hook['hook_method']);
                     }
 
                     // write to the session to speed up performance
@@ -207,8 +174,14 @@ class LogicHook
                 }
 
             }
+
+            //merge $SpiceCRMHooks into $hook_array
+            foreach($SpiceCRMHooks as $event => $hook){
+                foreach($hook as $idx => $hk)
+                    $hook_array[$event][] = $hk;
+            }
         }
-        return array_merge($hook_array, $SpiceCRMHooks);
+        return $hook_array;
     }
 
 	public function getHooks($module_dir, $refresh = false)
