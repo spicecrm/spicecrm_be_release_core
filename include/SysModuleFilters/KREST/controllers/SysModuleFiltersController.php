444339cf1bb9be0d8e66fda69563d36016ab965d
maretval <valerie.maret@twentyreasons.com>
2019-05-30 19:07:56 +0200

    Fix modulefilters

--- include/SysModuleFilters/KREST/controllers/SysModuleFiltersController.php
+++ include/SysModuleFilters/KREST/controllers/SysModuleFiltersController.php
@@ -3,22 +3,22 @@
 * This file is part of SpiceCRM. SpiceCRM is an enhancement of SugarCRM Community Edition
 * and is developed by aac services k.s.. All rights are (c) 2016 by aac services k.s.
 * You can contact us at info@spicecrm.io
-* 
+*
 * SpiceCRM is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version
-* 
+*
 * The interactive user interfaces in modified source and object code versions
 * of this program must display Appropriate Legal Notices, as required under
 * Section 5 of the GNU Affero General Public License version 3.
-* 
+*
 * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
 * these Appropriate Legal Notices must retain the display of the "Powered by
 * SugarCRM" logo. If the display of the logo is not reasonably feasible for
 * technical reasons, the Appropriate Legal Notices must display the words
 * "Powered by SugarCRM".
-* 
+*
 * SpiceCRM is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -53,8 +53,8 @@ class SysModuleFiltersController
         $this->checkAdmin();
 
         $filters = [];
-        $filtersObj = "SELECT 'global' As `type`, fltrs.* FROM sysmodulefilters fltrs  WHERE fltrs.module = '{$args['module']}' UNION ";
-        $filtersObj .= "SELECT 'custom' As `type`, cfltrs.* FROM syscustommodulefilters cfltrs  WHERE cfltrs.module = '{$args['module']}'";
+        $filtersObj = "SELECT 'global' As `scope`, fltrs.* FROM sysmodulefilters fltrs  WHERE fltrs.module = '{$args['module']}' UNION ";
+        $filtersObj .= "SELECT 'custom' As `scope`, cfltrs.* FROM syscustommodulefilters cfltrs  WHERE cfltrs.module = '{$args['module']}'";
         $filtersObj = $db->query($filtersObj);
         while ($filter = $db->fetchByAssoc($filtersObj))
             $filters[] = $filter;
@@ -68,15 +68,16 @@ class SysModuleFiltersController
         $this->checkAdmin();
         $filterdata = $req->getParsedBody();
         // check if filter exists
-        $filter = $db->fetchByAssoc($db->query("SELECT * FROM sysmodulefilters WHERE id='{$args['filter']}'"));
+        $table = $filterdata['scope'] == 'custom' ? 'syscustommodulefilters' : 'sysmodulefilters';
+        $filter = $db->fetchByAssoc($db->query("SELECT * FROM $table WHERE id='{$args['filter']}'"));
         if ($filter) {
             $filterdefs = json_encode($filterdata['filterdefs']);
-            $db->query("UPDATE sysmodulefilters SET name='{$filterdata['name']}', filterdefs='$filterdefs', version='{$filterdata['version']}', package='{$filterdata['package']}' WHERE id = '{$args['filter']}'");
+            $db->query("UPDATE $table SET name='{$filterdata['name']}', filterdefs='$filterdefs', version='{$filterdata['version']}', package='{$filterdata['package']}' WHERE id = '{$args['filter']}'");
 
             $this->setCR('I', $args['filter'], "{$args['module']}/{$filterdata['name']}");
         } else {
             $filterdefs = json_encode($filterdata['filterdefs']);
-            $db->query("INSERT INTO sysmodulefilters (id, created_by_id, module, name, filterdefs, version, package) VALUES('{$args['filter']}', '$current_user->id', '{$args['module']}', '{$filterdata['name']}', '$filterdefs', '{$filterdata['version']}', '{$filterdata['package']}')");
+            $db->query("INSERT INTO $table (id, created_by_id, module, name, filterdefs, version, package) VALUES('{$args['filter']}', '$current_user->id', '{$args['module']}', '{$filterdata['name']}', '$filterdefs', '{$filterdata['version']}', '{$filterdata['package']}')");
 
             $this->setCR('I', $args['filter'], "{$args['module']}/{$filterdata['name']}");
         }
@@ -87,11 +88,11 @@ class SysModuleFiltersController
         global $db;
         $this->checkAdmin();
         $id = $db->quote($args['filter']);
-        $result = $db->query("DELETE FROM sysmodulefilters WHERE id = '$id'");
-
+        $resultGlobal = $db->query("DELETE FROM sysmodulefilters WHERE id = '$id'");
+        $resultCustom = $db->query("DELETE FROM syscustommodulefilters WHERE id = '$id'");
         $this->setCR('D', $id);
 
-        return $res->withJson($result);
+        return $res->withJson($resultCustom && $resultGlobal);
     }
 
     /**
