e8ece7c5d057f6491d248d7a588fe649ad4eb48f
maretval <valerie.maret@twentyreasons.com>
2019-10-03 07:11:43 +0200

    Preparing release 201909001 -  fix roles configurator

--- modules/SystemUI/KREST/extensions/systemui.php
+++ modules/SystemUI/KREST/extensions/systemui.php
@@ -138,6 +138,23 @@ $app->group('/spiceui', function () use ($app, $uiRestHandler) {
                 echo json_encode($uiRestHandler->deleteModelValidation($args['id']));
             });
         });
+
+            $app->group('/roles', function () use ($app, $uiRestHandler) {
+
+                $app->get('/{userid}', function ($req, $res, $args) use ($app, $uiRestHandler) {
+                    echo json_encode($uiRestHandler->getAllRoles($args['userid']));
+                });
+                $app->post('/{roleid}/{userid}/{default}', function ($req, $res, $args) use ($app, $uiRestHandler) {
+                    global $current_user;
+                    if (!$current_user->is_admin) throw (new KREST\ForbiddenException('No administration privileges.'))->setErrorCode('notAdmin');
+                    echo json_encode($uiRestHandler->setUserRole($args));
+                });
+                $app->delete('/{roleid}/{userid}', function ($req, $res, $args) use ($app, $uiRestHandler) {
+                    global $current_user;
+                    if (!$current_user->is_admin) throw (new KREST\ForbiddenException('No administration privileges.'))->setErrorCode('notAdmin');
+                    echo json_encode($uiRestHandler->deleteUserRole($args));
+                });
+            });
     });
     $app->group('/admin', function () use ($app, $uiRestHandler) {
         $app->get('/navigation', function ($req, $res, $args) use ($app, $uiRestHandler) {
