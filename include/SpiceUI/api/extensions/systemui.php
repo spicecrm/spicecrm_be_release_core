17cce4efc4658a58344ba0686a838ff77b3323f2
maretval <valerie.maret@twentyreasons.com>
2021-07-15 19:59:47 +0200

    HotFix 1000623 Parameters description for route /configuration/spiceui/core/modelvalidations

--- include/SpiceUI/api/extensions/systemui.php
+++ include/SpiceUI/api/extensions/systemui.php
@@ -1,31 +1,31 @@
 <?php
 /*********************************************************************************
-* This file is part of SpiceCRM. SpiceCRM is an enhancement of SugarCRM Community Edition
-* and is developed by aac services k.s.. All rights are (c) 2016 by aac services k.s.
-* You can contact us at info@spicecrm.io
-* 
-* SpiceCRM is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version
-* 
-* The interactive user interfaces in modified source and object code versions
-* of this program must display Appropriate Legal Notices, as required under
-* Section 5 of the GNU Affero General Public License version 3.
-* 
-* In accordance with Section 7(b) of the GNU Affero General Public License version 3,
-* these Appropriate Legal Notices must retain the display of the "Powered by
-* SugarCRM" logo. If the display of the logo is not reasonably feasible for
-* technical reasons, the Appropriate Legal Notices must display the words
-* "Powered by SugarCRM".
-* 
-* SpiceCRM is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-********************************************************************************/
+ * This file is part of SpiceCRM. SpiceCRM is an enhancement of SugarCRM Community Edition
+ * and is developed by aac services k.s.. All rights are (c) 2016 by aac services k.s.
+ * You can contact us at info@spicecrm.io
+ *
+ * SpiceCRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo. If the display of the logo is not reasonably feasible for
+ * technical reasons, the Appropriate Legal Notices must display the words
+ * "Powered by SugarCRM".
+ *
+ * SpiceCRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ********************************************************************************/
 
 use SpiceCRM\includes\RESTManager;
 use SpiceCRM\includes\Middleware\ValidationMiddleware;
@@ -637,6 +637,12 @@ $routes = [
                 'type' => ValidationMiddleware::TYPE_COMPLEX,
                 'example' => '[{}]',
             ],
+            'deleted' => [
+                'in' => 'body',
+                'description' => 'the priority of the rule',
+                'type' => ValidationMiddleware::TYPE_BOOL,
+                'example' => '0',
+            ],
             'isnewrecord' => [
                 'in' => 'body',
                 'description' => 'indicates if the validation rule is new',
@@ -666,6 +672,29 @@ $routes = [
                 'description' => 'the name of the event to apply the rule',
                 'type' => ValidationMiddleware::TYPE_STRING,
                 'example' => 'all',
+            ],
+            'priority' => [
+                'in' => 'body',
+                'description' => 'the priority of the rule',
+                'type' => ValidationMiddleware::TYPE_NUMERIC,
+                'example' => '0',
+            ]
+        ]
+    ],
+    [
+        'method' => 'delete',
+        'route' => '/configuration/spiceui/core/modelvalidations/{id}',
+        'oldroute' => '/spiceui/core/modelvalidations',
+        'class' => SystemUIController::class,
+        'function' => 'SystemDeleteModelValidation',
+        'description' => 'deletes a model validation',
+        'options' => ['noAuth' => false, 'adminOnly' => true, 'validate' => true],
+        'parameters' => [
+            'id' => [
+                'in' => 'query',
+                'description' => 'the id of the validation rule',
+                'type' => ValidationMiddleware::TYPE_GUID,
+                'example' => '894562d5-d74b-4587-a10a-fabe7ec2f696',
             ]
         ]
     ],
